7cb7f69e7ba68abcad4e66548aac0a9af3a0b334
Mateo <143887515+MateoFP@users.noreply.github.com>
2023-11-01 14:50:55 +0100

    Update credentials.py

--- esp32_project1/credentials.py
+++ esp32_project1/credentials.py
@@ -1,9 +1,9 @@
 
 credentials = {
-    'ssid' : 'Phone123',
-    'password' : 'ax102947',
+    'ssid' : 'x',
+    'password' : 'x',
     'ADAFRUIT_IO_URL' : b'io.adafruit.com',
-    'ADAFRUIT_USERNAME' : b'MateoFP',
-    'ADAFRUIT_IO_KEY' : b'aio_hBOI07fmqTYj0lkpSRWMdsagFaby',
+    'ADAFRUIT_USERNAME' : b'x',
+    'ADAFRUIT_IO_KEY' : b'x',
     'ADAFRUIT_IO_FEEDNAME' : b'esp32feed'
     }
